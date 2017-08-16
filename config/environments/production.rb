d9f9b2127c915cbbe90f2fab7cf2c4f62a26ad06
mark miranda <notmarkmiranda@gmail.com>
2017-08-16 13:53:38 -0600

    update env calls

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,10 +91,10 @@ Rails.application.configure do
   config.paperclip_defaults = {
     storage: :s3,
     s3_credentials: {
-      bucket: ENV.fetch('S3_BUCKET'),
-      access_key_id: ENV.fetch('S3_KEY'),
-      secret_access_key: ENV.fetch('S3_SECRET'),
-      s3_region: ENV.fetch('S3_REGION'),
+      bucket: ENV['S3_BUCKET'],
+      access_key_id: ENV['S3_KEY'],
+      secret_access_key: ENV['S3_SECRET'],
+      s3_region: ENV['S3_REGION'],
     }
   }
 end
